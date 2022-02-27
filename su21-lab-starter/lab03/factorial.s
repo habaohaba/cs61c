2570e817fc2db1e4e54c9daf8772f0826fef88c8
habaohaba <15313137931lz@gmail.com>
2022-02-27 17:17:47 +0800

    finish lab 03

--- su21-lab-starter/lab03/factorial.s
+++ su21-lab-starter/lab03/factorial.s
@@ -22,3 +22,13 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    addi t0, a0, -1 # save N-1 value to t0
+    add t1, a0, x0 # t1 save factorial result, default is N
+loop:
+    beq t0, x0, exit # end loop if n = 0
+    mul t1, t1, t0 # calculate factorial
+    addi t0, t0, -1 # n-1
+    j loop
+exit:
+    add a0, t1, x0
+	jr ra
